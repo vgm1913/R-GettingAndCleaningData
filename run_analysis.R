b79d82a0d7c74ff8587e7b83929eaf6800b02fc4
vgm1913 <v.g.m.1913@gmail.com>
2014-12-15 18:57:13 -0800

    Update run_analysis.R
    Needs to get cleaned up - but looks like it's working. Also need to do a manual check to make sure the mean by measure is correct

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,50 @@
-## script: run_analysis.R
-## author: VGM
-## created: DEC-08-2014
-## coursera: https://class.coursera.org/getdata-016/human_grading
-## run_analysis is a utility function that is specifically designed to work with UCI HAR dataset
-## HAR = Human Activity Recognition using smart phones Dataset
-## (see http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
-## & performs the following:
-##	1. Merges the training and the test sets to create one data set
-##  2. Extracts only the measurements on the mean and standard deviation for each measurement
-##  3. Uses descriptive activity names to name the activities in the data set + Appropriately labels the data set with descriptive variable names
-##  4. From the data set in previous step, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+##
+## script:        run_analysis.R
+## author:        VGM
+## created:       DEC-08-2014
+## last modified: DEC-15-2014
+## coursera:      https://class.coursera.org/getdata-016/human_grading
+##
+## SUMMARY:       run_analysis is a utility function that is specifically designed to work with UCI HAR dataset
+##                HAR = Human Activity Recognition using smart phones Dataset
+##                (see http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+##                & performs the following:
+##
+##              	1. Merges the training and the test sets to create one data set
+##                2. Extracts only the measurements on the mean and standard deviation for each measurement
+##                3. Uses descriptive activity names to name the activities in the data set + Appropriately labels
+##                   the data set with descriptive variable names
+##                4. From the data set in previous step, creates a second, independent tidy data set
+##                   with the average of each variable for each activity and each subject
 ##
 ## Assumptions & Meta-Data information
 ## -----------------------------------
-## A) all data is stored under current working directory
+## A) all data is stored under ./data directory under the current working directory of the script
 ## B) activity_labels.txt contains the labels for measured Human Activities - use these labels in association with y_train.txt or y_test.txt files
-## C) features.txt        contains the labels for columns for the data measurements stored in the test or train sub-directories under X_train.txt & X_test.txt files
-library(data.table)
-library(reshape2)
+## C) features.txt contains the labels for columns for the data measurements stored in the test or train sub-directories under X_train.txt & X_test.txt files
+## D) run_analyis.R script outputs to the local working directory to a file named: run_analysis-Activity_Subject_Measurement_Means.txt (see README.TXT)
+##
+
+## INITIALIZE packages & declare dependencies
+if (!require("data.table")) {
+  install.packages("data.table")
+}
+
+if (!require("reshape2")) {
+  install.packages("reshape2")
+}
+
+require("data.table")
+require("reshape2")
+
+## run_analysis.R CONFIGURATION PARAMTERS
+##
+DIR_data        <- "data"
+ZIP_FILE_data   <- "UCI_HAR.zip"
+FILE_activities <- paste (DIR_data, "/activity_labels.txt", sep="")
+FILE_features   <- paste (DIR_data, "/features.txt", sep="")
+FILE_output     <- "./run_analysis-Activity_Subject_Measurement_Means.txt"
+
 ## ------------------ Helper Functions -------------------
 load_and_label <- function (file1, new_colnames, file2 = "") {
   if (file2 == "") {
@@ -42,30 +69,46 @@ add_id_column <- function (data_table) {
 
 transform_column_names <- function (char_vector) {
   x <- gsub (",", "-", char_vector)
-  x <- gsub ("^t", "Time", x)
-  x <- gsub ("^f", "Freq", x)
+#  x <- gsub ("^t", "time-", x) - not needed for assignment
+#  x <- gsub ("^f", "freq-", x) - not needed for assignment
   x
 }
+
 ## ------------------ Begin Script -----------------------
 ## Stage 0) Download and set up Meta Data information
 ## Step 0.1 - download the dataset from UCI web-site - Note: URL for this assignment is hard-wired (in future it should be a configuration parameter)
-if (!file.exists("UCI_HAR.zip")) {
+if (!file.exists(DIR_data)) {
+  print(paste("Could not file ", DIR_data, " directory - creating the directory..."))
+  dir.create(DIR_data)
+}
+
+if (!file.exists(ZIP_FILE_data)) {
 	print("UCI HAR data zip find not found - please wait while download completes...")
 	fileURL <- "http://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip"
-	download.file(fileURL, destfile = "UCI_HAR.zip")
+	download.file(fileURL, destfile = ZIP_FILE_data)
 }
-if (!file.exists("features.txt")) {
+
+if (!file.exists(FILE_features)) {
 	print("unzipping the content of UCI HAR zip file, please wait...")
-	unzip("UCI_HAR.zip", files = NULL, list = FALSE, overwrite = TRUE,
-      junkpaths = TRUE, exdir = ".", unzip = "internal",
+	unzip(ZIP_FILE_data, files = NULL, list = FALSE, overwrite = TRUE,
+      junkpaths = TRUE, exdir = DIR_data, unzip = "internal",
       setTimes = FALSE)
 }
+
 ## Step 0.2 - read Meta data information into R as tables for reference & processing
 print ("Stage 0) Initializing and loading Meta data...")
-DT_act_labels <- load_and_label("activity_labels.txt", c("act_id", "Activity"))
+if (!file.exists(FILE_activities)) {
+  stop (paste("run_analysis.R ERROR: could not find:", FILE_activities))
+}
+DT_act_labels <- load_and_label(FILE_activities, c("act_id", "Activity"))
 setkey(DT_act_labels, act_id)
-DT_feat_labels <- load_and_label("features.txt", c("feat_id", "feat_label"))
+
+if (!file.exists(FILE_features)) {
+  stop (paste("run_analysis.R ERROR: could not find:", FILE_features))
+}
+DT_feat_labels <- load_and_label(FILE_features, c("feat_id", "feat_label"))
 setkey(DT_feat_labels, feat_id)
+
 ##
 ## Stage 1) Merge training and test sets into one data set
 ##
@@ -94,6 +137,7 @@ DT_HAR_feat <- add_id_column (
                 , "X_train.txt")
   )
 setkey (DT_HAR_feat, id_seq)
+
 ##
 ## Stage 2) Extract only the measurements on the mean and stadard deviation for each measurement
 ##
@@ -120,31 +164,28 @@ DT_HAR_data <- merge (DT_HAR_data,
 DT_HAR_data[, act_id := NULL] # remove it, we replaced it with the full form name of activity
 DT_HAR_data[, id_seq := NULL] 
 
-## Step 3.2 - tidy the resulting data via melt to make sure each measured value is appropriatly labeled 
-tidy_data_ids <- c("Subject", "Activity")
-measurement_variables <- colnames(DT_HAR_data)
-measurement_variables <- measurement_variables[! measurement_variables %in% tidy_data_ids]
-DT_HAR_tidy <- melt(DT_HAR_data, id=tidy_data_ids, measure.vars=measurement_variables)
-
-## Step 3.3 - clean up the data and remove data and variable no longer needed
+## Step 3.2 - clean up the data and remove data and variable no longer needed
 # remove these, we don't need it anymore
+rm(col)
 rm(DT_HAR_feat)
 rm(DT_act_labels)
 rm(DT_act_data)
 rm(DT_feat_col_to_remove)
 rm(DT_feat_labels)
 rm(feat_col_to_remove)
-rm(measurement_variables)
-rm(DT_HAR_data)
-rm(col)
 ##
 ## Stage 4) From the data set in previous step, creates a second, independent tidy data set with the average of each variable for each activity and each subject
 ##
 print ("Stage 4) Creating a second, independent tidy data set with the average of each variable for each activity and each subject")
 ## Step 4.1 - make a new tidy data set with the average of each variable for each activity and each subject
+DT_HAR_melt <- melt(DT_HAR_data, id=c("Activity", "Subject"))
+DT_HAR_tidy <- dcast(DT_HAR_melt, Activity + Subject ~ variable, mean)
 
-## Step 4.2 - write out the resulting tidy data to a file in the current working directory
+## Step 4.2 - write out the resulting tidy data to the file specified in README.TXT into current working directory
+write.csv(DT_HAR_tidy, "run_analysis-Activity_Subject_Measurement_Means.txt")
 
 ## Step 4.3 - final clean up
-rm(tidy_data_ids)
+rm(DT_HAR_data)
+rm(DT_HAR_melt)
+rm(DT_HAR_tidy)
 ## ------------------ End Script -----------------------
